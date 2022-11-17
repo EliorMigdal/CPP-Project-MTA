ee73d28e298fccfff7c164af7209dd8fd3bc13d6
EliorMigdal <eliormigdal7@gmail.com>
2022-11-17 21:37:44 +0200

    Added a basic test on main.cpp. Please run on VS and check if it works

--- CPP_files/main.cpp
+++ CPP_files/main.cpp
@@ -2,5 +2,18 @@
 #include "../Headers/Menu.h"
 
 int main() {
+    Menu mainMenu;
+    int userDecision;
+    cout << "Welcome to our social network!" << endl;
+    mainMenu.printMenu();
+    cout << "Please choose your action:" << endl;
+    cin >> userDecision;
+
+    while (userDecision != 12)
+    {
+        mainMenu.setDesicion(userDecision);
+        cout << "Please choose another action:" << endl;
+        cin >> userDecision;
+    }
     return 0;
 }
